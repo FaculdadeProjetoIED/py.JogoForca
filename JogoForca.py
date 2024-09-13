32cf02b415bde5aeaba58d4feadb19d7f7b1f995
Vitor Oliveira <vitorsimon04@gmail.com>
2024-09-13 13:36:44 -0300

    FEAT - Definições

---
+++ JogoForca.py
@@ -0,0 +1,11 @@
+# Definições
+alvo = "hangman"
+letras_usadas = []
+tentativas = 0
+# Variável para definir a quantidade de erros do usuário
+tentativas_falhas = 0
+acertou = False
+# Variável para definir se a dica será exibida ou não
+dica_ativa = True
+# Variável para definir a string da dica para poder ser impressa
+dica_mensagem = ""
