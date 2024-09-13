b692f2b79713aa129f1f1090ca1d0bab0d7dc3cc
Vitor Oliveira <vitorsimon04@gmail.com>
2024-09-13 13:37:05 -0300

    FEAT - Função principal

--- JogoForca.py
+++ JogoForca.py
@@ -9,3 +9,14 @@ acertou = False
 dica_ativa = True
 # Variável para definir a string da dica para poder ser impressa
 dica_mensagem = ""
+
+
+
+# Função principal do jogo da forca
+def main():
+    print("Olá mundo!")
+
+# Verifica se o programa é executado diretamente
+if __name__ == "__main__":
+    # Chama a função principal
+    main()
