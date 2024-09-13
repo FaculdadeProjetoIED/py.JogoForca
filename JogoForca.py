dd5c7fa5a985a61fc4a4db2a0792cd85d519701c
Vitor Oliveira <vitorsimon04@gmail.com>
2024-09-13 13:41:22 -0300

    FEAT - Importações e função para limpar o terminal

--- JogoForca.py
+++ JogoForca.py
@@ -1,3 +1,7 @@
+# Importações
+from msvcrt import getch # Importação do Getch para a captura da tecla de confirmação do usuário
+import os # Importação do os para que o terminal possa ser limpo
+
 # Definições
 alvo = "hangman"
 letras_usadas = []
@@ -9,6 +13,14 @@ acertou = False
 dica_ativa = True
 # Variável para definir a string da dica para poder ser impressa
 dica_mensagem = ""
+
+
+
+# Função para limpar o terminal
+def limpar_terminal():
+    print("Pressione 'ENTER'para continuar.")
+    getch() # Captura a tecla de confirmação pessioanda pelo usuário
+    os.system('cls') # cls, pois a máquina de desenvolvimento é windows
 
 
 
