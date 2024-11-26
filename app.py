ddb0eae57627f1765a1d2ec9e87d75a71a292dd0
Vitor Oliveira <vitorsimon04@gmail.com>
2024-11-25 23:09:03 -0300

    FIX - Criação de arquivo único para melhor configuração no colab

--- app.py
+++ app.py
@@ -1,137 +1,202 @@
 # ==================== Importações e definições gerais ====================
 # Importações
-from msvcrt import getch # Importação do Getch para a captura da tecla de confirmação do usuário
-import os # Importação do os para que o terminal possa ser limpo
-from JogoDaForca.JogoForca import jogo_forca # Importação do jogo da forca para ser inicializado
+from msvcrt import getch  # Importação do Getch para a captura da tecla de confirmação do usuário
+import os  # Importação do os para que o terminal possa ser limpo
+import requests # Importação para poder obter as palavras
+import random  # Importação para poder sortear as palavras
+from unidecode import unidecode  # Importação para remover acentuação das letras e palavras
+
+# ==================== Controle - jogo da forca ====================
 
 # Função para limpar o terminal
 def limpar_terminal():
-    print("\nPressione 'ENTER'para continuar.")
-    getch() # Captura a tecla de confirmação pessioanda pelo usuário
-    os.system('cls') # cls, pois a máquina de desenvolvimento é windows
+    print("\nPressione 'ENTER' para continuar.")
+    getch()  # Captura a tecla de confirmação pressionada pelo usuário
+    os.system('cls')  # cls, pois a máquina de desenvolvimento é windows
 
 
-# Função para processar a categoria escolhida pelo usuário
-def processa_categoria_escolhida(categoria):
-    
-    # Dicionário com as categorias mapeadas
-    categorias = {
-        1: "Países",
-        2: "Verbos",
-        3: "Animais",
-        4: "Frutas"
-    }
-    
-    # Se a entrada do usuário for um número válido
-    if categoria in [0, 1, 2, 3, 4]:
-        # Verifica se não é a opção de voltar ao menu
-        if not categoria == 0:
-            # Obtem a categoria no dicionário de acordo com o valor
-            nome_categoria = categorias.get(categoria)
-            limpar_terminal() 
-            # Função para iniciar o jogo, passando a categoria como parametro
-            jogo_forca(nome_categoria)
-        else:
-            print("Voltando para o menu")
-            limpar_terminal()
-            # Encerra o loop, voltando assim para o menup principal
-            return False
-    else:
-        print("\n\nInsira uma opção válida.")
-        limpar_terminal()
-        # Mantem o loop ativo
-        return True
-
-
-def obtem_categoria():
-    print("========== CATEGORIAS ==========")
-    print("\nVoltar ao menu = 0;")
-    print("\nPaíses = 1;")
-    print("Verbos = 2;")
-    print("Animais = 3;")
-    print("Frutas = 4.")
-
-    # Try adicionado para capturar erros na conversão da entrada do usuário para um inteiro
+# Obtém uma palavra aleatória para o jogo usando a API.
+def nova_palavra():
     try:
-        # Define categoria com o input do usuário
-        categoria = int(input("\nEscolha uma opção: "))
-    except:
-        print("\n\nInsira uma opção válida.")
-        limpar_terminal()
-        # Mantem o loop ativo
+        # Faz a requisição diretamente ao endpoint
+        resposta = requests.get("https://faccamp.pythonanywhere.com/hangman-api/getword")
+
+        # Converte a resposta JSON em um dicionário Python
+        dado = resposta.json()
+
+        # Obtém a palavra do campo correto no JSON retornado
+        palavra = dado.get("palavra")
+        if palavra:
+            return palavra
+        else:
+            print("Erro: o campo 'palavra' não foi encontrado na resposta da API.")
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print("Erro na conexão com a API: ", e)
+        return None
+
+
+# Função para exibir quantidade de letras da palavra
+def dica_atualizar():
+    global dica_mensagem, alvo  # Variáveis globais para manipulação dentro da função
+    dica_mensagem = ""
+    for char in alvo:
+        if char in letras_usadas:
+            dica_mensagem += char + ' '
+        elif char.isalpha():
+            dica_mensagem += "_ "
+        else:
+            dica_mensagem += " "
+    dica_mensagem = f"A palavra tem {len(alvo)} letras.\n\n{dica_mensagem}"
+
+
+# Função para validar a entrada (input, 'menu_opcao') do usuário
+def validacao_input_usuario(menu_opcao):
+    if menu_opcao in letras_usadas:
+        print("Insira uma letra não usada anteriormente.")
+        return False
+    elif len(menu_opcao) == 1 and not menu_opcao.isdigit():
         return True
-    
-    return processa_categoria_escolhida(categoria)
+    else:
+        if len(menu_opcao) != 1:
+            print("Insira apenas uma letra.")
+        else:
+            print("Opção inválida")
+        return False
+
+
+# Função para verificar se a letra está na palavra do 'alvo'
+def verificacao_palpite(menu_opcao):
+    global tentativas_falhas
+    if menu_opcao in alvo:
+        print(f"\n\nA letra '{menu_opcao}' está na palavra!")
+        dica_atualizar()
+        if verificacao_vitoria():
+            return
+    else:
+        print(f"\n\nA letra '{menu_opcao}' não está na palavra, tente novamente!")
+        tentativas_falhas += 1
+
+
+# Função para exibir o resultado do jogo
+def exibe_resultado(acertou):
+    if acertou:
+        print("\n\nParabéns. Você ganhou!")
+        print(f"Com {tentativas} tentativas.")
+    else:
+        print("\n\nPerdeu!")
+        print("A palavra era:", alvo)
+
+
+# Função para verificar se o usuário ganhou o jogo
+def verificacao_vitoria():
+    global acertou
+    todas_letras_validadas = all(
+        letra.isalpha() and letra in letras_usadas for letra in alvo)
+    if todas_letras_validadas:
+        limpar_terminal()
+        acertou = True
+        exibe_resultado(acertou)
+        return True
+    else:
+        return False
 
 
 # Função para processar a escolha do usuário
-def processa_escolha_usuario(menu_opcao):
+def processa_escolha_usuario(menu_opcao, acertou):
+    global dica_mensagem, dica_ativa, tentativas
+    if tentativas_falhas >= 5:
+        verificacao_palpite(menu_opcao)
+        limpar_terminal()
+        exibe_resultado(False)
+        return False
+    elif menu_opcao == "1":
+        dica_atualizar()
+        print(f"\n\n{dica_mensagem}")
+        dica_ativa = True
+        limpar_terminal()
+    elif menu_opcao == "2":
+        print("\nVocê desistiu do jogo.")
+        return False
+    elif validacao_input_usuario(menu_opcao):
+        letras_usadas.append(menu_opcao)
+        tentativas += 1
+        verificacao_palpite(menu_opcao)
+        if verificacao_vitoria():
+            return False
+        limpar_terminal()
+    else:
+        limpar_terminal()
+    return not acertou
+
+
+# Função principal do jogo da forca
+def jogo_forca():
+    global alvo, tentativas, tentativas_falhas, letras_usadas, dica_mensagem, dica_ativa, acertou
     
-    # Se a entrada do usuário for um número válido
+    # Obtém a palavra da API
+    palavra_api = nova_palavra()
+    if palavra_api is None:
+        print("Não foi possível obter uma palavra para o jogo.")
+        return
+    
+    alvo = unidecode(palavra_api.lower())  # Remove acentos e converte para minúsculas
+    tentativas, tentativas_falhas, letras_usadas = 0, 0, []
+    dica_ativa, acertou = False, False
+    dica_atualizar()
+    
+    jogando = True
+    while jogando:
+        print("\n\nMenu do Jogo")
+        print(f"Dica: {dica_mensagem}")
+        menu_opcao = input("\nDigite uma letra (ou 1 para dica, 2 para desistir): ")
+        jogando = processa_escolha_usuario(menu_opcao, acertou)
+
+
+# Função para processar a escolha do usuário no menu principal
+def processa_escolha_usuario_menu(menu_opcao):
     if menu_opcao in [1, 2]:
-        # Se o usuário escolher a opção igual a 1, o jogo inicia
         if menu_opcao == 1:
-            limpar_terminal()            
-            # Inicia o jogo da forca
-            # A função obter categoria, traz as opções disponíveis dentro da categoria selecionada na função
-            menu_categoria_loop= True
-            while menu_categoria_loop:
-                # Define loop com o valor retornado da função 'obtem_categoria()'
-                menu_categoria_loop = obtem_categoria()
-            # Mantem o loop ativo
+            limpar_terminal()
+            menu_principal_loop = True
+            while menu_principal_loop:
+                menu_principal_loop = jogo_forca()
             return True
-        # Se o usuário escolher a opção igual a 2, o jogo é encerrado
         elif menu_opcao == 2:
             limpar_terminal()
             print("\nVocê escolheu sair do jogo.")
-            # Encerra o loop
             return False
-        # Senão, avisa ao usuário que ele deve escolher uma opção válida
-        else:
-            print("\n\nInsira uma opção válida.")
-            limpar_terminal()
-            # Mantem o loop ativo
-            return True
-    # Senão, avisa ao usuário que ele deve escolher uma opção válida
     else:
         print("\n\nInsira uma opção válida.")
         limpar_terminal()
-        # Mantem o loop ativo
         return True
 
 
-# Função para exibir o menu e obter a escolha do usuário
+# Função para exibir o menu principal
 def menu():
     print("\n\n           JOGO DA FORCA - UNIFACCAMP           ")
     print("================================================")
     print("                      MENU                      \n")
     print("\nPara jogar, pressione 1;")
     print("Para sair do jogo, pressione 2.\n")
-    
-    # Try adicionado para capturar erros na conversão da entrada do usuário para um inteiro
     try:
-        # Define menu_opcao com o input do usuário
         menu_opcao = int(input("\nEscolha uma opção: "))
     except:
         print("\n\nInsira uma opção válida.")
         limpar_terminal()
-        # Mantem o loop ativo
         return True
-    
-    return processa_escolha_usuario(menu_opcao)
+    return processa_escolha_usuario_menu(menu_opcao)
 
 
-# Função principal do jogo da forca
+# Função principal
 def main():
-    os.system('cls') # cls, pois a máquina de desenvolvimento é windows
-    # Define a variável menu_loop como True para poder iniciar o loop de interação com o usuário
+    os.system('cls')  # cls, pois a máquina de desenvolvimento é windows
     menu_loop = True
-    # Loop para chamar o menu após cada opção
     while menu_loop:
-        # Define loop com o valor retornado da função 'menu()'
         menu_loop = menu()
 
 
 # Executa a função main
-if(__name__ == "__main__"):
+if __name__ == "__main__":
     main()
